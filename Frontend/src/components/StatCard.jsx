11c9845be952b341000403fd90d76d7a72c95a28
Abir <166408074+abirdj1@users.noreply.github.com>
2025-04-11 16:45:48 +0200

    statcard

--- Frontend/src/components/StatCard.jsx
+++ Frontend/src/components/StatCard.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import "../styles/StatCard.css";
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+import { HiOutlineDatabase } from "react-icons/hi";
+import "../styles/StatCard.css";
 
 export default function StatCard({ title, value, change, active }) {
   return (
     <div className="stat-card">
-      <div className="title">
-        {title}
-        <span className="icon-status">
-          {active ? <FaArrowUp color="green" /> : <FaArrowDown color="red" />}
-        </span>
+      <div className="header">
+        <div className="title-section">
+          <div className="title">{title}</div>
+          <div className="value">{value}</div>
+        </div>
+        <div className="icon-box">
+          <HiOutlineDatabase className="icon" />
+        </div>
       </div>
-      <div className="value">{value}</div>
       <div className={`change ${active ? "up" : "down"}`}>
+        <FaArrowUp color="green" />
         {change} this week
       </div>
     </div>
